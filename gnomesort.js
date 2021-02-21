325c2e4363558329db3f48e128e91e658f031cb1
unknown <budiarto.salim91@gmail.com>
2021-02-21 13:05:27 +0700

    turns out it was indeed insertion, gnomesort not made yet

--- gnomesort.js
+++ gnomesort.js
@@ -2,21 +2,10 @@ const fs = require('fs');
 let data = JSON.parse(fs.readFileSync('./data/datasets/random.json', 'utf-8'));
 
 console.time("Finishes sorting in");
-let sorted = insertionSort(data);
+let sorted = gnomeSort(data);
 console.timeEnd("Finishes sorting in");
 fs.writeFileSync('./data/results.json', JSON.stringify(sorted, null, 2));
 
-function insertionSort(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = i + 1; j > 0; j--) {
-      if (arr[j] < arr[j - 1]) {
-        let temp = arr[j];
-        arr[j] = arr[j - 1];
-        arr[j - 1] = temp;
-      } else {
-        break;
-      }
-    }
-  }
-  return arr;
+function gnomeSort(arr) {
+
 }
